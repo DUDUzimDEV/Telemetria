56626635bfc5d046b106d001627a221e9ccfca76
DUDUzimDEV <eduardo.b.vieira8@aluno.senai.br>
2026-02-26 14:19:42 -0300

    Telemetria2

--- setup/urls.py
+++ setup/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers, permissions
 from api_telemetria.api import viewsets
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API para telemetria de veiculos agricolas",
+        default_version="v1",
+        description="Sistema para cadastro e controle por telemetria de frota de veiculos agricolas",
+        terms_of_service="https://www.google.com/terms",
+        contact=openapi.Contact(email="eduardobonfim016@gmail.com"),
+        license=openapi.License(name="Free"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 route = routers.DefaultRouter()
 route.register(r'Marca', viewsets.MarcaViewset, basename="Marca")
@@ -30,4 +45,10 @@ route.register(r'UnidadeMedida', viewsets.UnidadeMedidaViewset, basename="Unidad
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
+]
+
+urlpatterns += [
+    path('swaggersjson/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swaggers/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
